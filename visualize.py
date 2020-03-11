47c314f40f39442e44d1ddeac4e35f3b14780101
mmyeditha <myeditha7@gmail.com>
2020-03-11 18:09:39 -0400

    ability to apply mask to an image and see what type of object is masked

--- visualize.py
+++ visualize.py
@@ -1,9 +1,59 @@
 import cv2
+import numpy as np
 
-def draw(img):
+
+def create_mask():
+    img = cv2.imread('ting.jfif')
+    cv2.rectangle(img, (0, 0), (img.shape[1], img.shape[0]), (0, 0, 0), -1)
+    cv2.rectangle(img, (0, 0), (140, 170), (255, 255, 255), -1)
+    maskedimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return maskedimg
+
+
+def draw_bounds(img, mask):
+    processed = cv2.imread(img)
+    fg = cv2.bitwise_and(processed, processed, mask=mask)
+    cv2.imshow('image', fg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def iterate():
+    a = [[1, 2, 3], [4, 5, 6]]
+    b = np.array(a)
+    for x in xrange(b.shape[0]):
+        for y in yrange(b.shape[1]):
+            if b[x][y] == 1: img[x][y][1] += 30
+            if b[x][y] == 2: img[x][y][0] += 30
+            if b[x][y] == 3: img[x][y][2] += 30
+            if b[x][y] == 4:
+                img[x][y][0] += 30
+                img[x][y][1] += 30
+            if b[x][y] == 5:
+                img[x][y][0] += 30
+                img[x][y][2] += 30
+            if b[x][y] == 6:
+                img[x][y][1] += 30
+                img[x][y][2] += 30
+            #if b[x][y] == 7:
+
+
+def test():
+    img = cv2.imread('ting.jfif')
+    for y in range(200, 500):
+        for x in range(300, 600):
+            img[x][y][0] += 75
     cv2.imshow('image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def draw_bounds(img, mask):
-    print('Balls')
+# 'car': 1,
+# 'person': 2,
+# 'rider': 3,
+# 'bus': 4,
+# 'bike': 5,
+# 'motor': 6,
+# 'truck': 7,
+# 'traffic light': 8,
+# 'train': 9,
+# 'traffic sign': 10,
