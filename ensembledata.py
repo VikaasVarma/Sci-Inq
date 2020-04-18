dbb28e35c98a590eed05477944c5dc6255f21942
VikaasVarma <vikaas.varma@gmail.com>
2020-04-18 17:01:56 -0400

    Fixed multiprocessing bug

--- ensembledata.py
+++ ensembledata.py
@@ -9,8 +9,65 @@ with warnings.catch_warnings():
     import os, time
     from data import Data
     from tqdm import tqdm
+    from functools import partial
     import threading
     from multiprocessing import Process, Manager
+
+def thread(start, end, df, index, thread_num, batch_size, total, n_threads):
+    columns = ['name', 'bounds']
+    data = Data()
+    process_df = [0] * 12
+    for i in range(len(process_df)):
+        process_df[i] = pd.DataFrame(np.nan, index = range((end - start) // 4), columns = columns)
+    with tqdm(total = end - start, desc = "Ensembling {0}".format(thread_num)) as pbar:
+        for j in range((end - start) // batch_size + 1):
+            data.get_data('train', start + j * batch_size, start + (j + 1) * batch_size if start + (j + 1) * batch_size < end else end)
+            for i, img in enumerate(data.x):
+                stats = [brightness(img), fourier_sharpness(img), canny_sharpness(img.astype(np.uint8))]
+                if -1 in stats:
+                    print("Failed at index {0} in batch {1}".format(i, j))
+                    errors.append(i, j)
+                    continue
+                k = None
+                if stats[0] < 0.203:
+                    if stats[1] < 0.172:
+                        if stats[2] < 0.0434:
+                            k = 0
+                        else:
+                            k = 1
+                    else:
+                        if stats[2] < 0.0434:
+                            k = 2
+                        else:
+                            k = 3
+                if stats[0] > 0.203 and stats[0] < 0.382:
+                    if stats[1] < 0.172:
+                        if stats[2] < 0.0434:
+                            k = 4
+                        else:
+                            k = 5
+                    else:
+                        if stats[2] < 0.0434:
+                            k = 6
+                        else:
+                            k = 7
+                if stats[0] > 0.382:
+                    if stats[1] < 0.172:
+                        if stats[2] < 0.0434:
+                            k = 8
+                        else:
+                            k = 9
+                    else:
+                        if stats[2] < 0.0434:
+                            k = 10
+                        else:
+                            k = 11
+                
+                process_df[k].loc[index[k]] = [data.names[i], data.bounds[i]]
+                index[k] += 1
+                pbar.update(1)
+    for i, d in enumerate(process_df):
+        df[i] = df[i].append(d.dropna(), ignore_index = True)
 
 def ensemble(batch_size, total = -1, n_threads = 1):
     if total == -1:
@@ -21,61 +78,6 @@ def ensemble(batch_size, total = -1, n_threads = 1):
     errors = []
     for i in range(len(classes)):
         classes[i] = pd.DataFrame(columns = columns)
-
-    def thread(start, end, df, index, thread_num):
-        data = Data()
-        process_df = [0] * 12
-        for i in range(len(process_df)):
-            process_df[i] = pd.DataFrame(np.nan, index = range((end - start) // 4), columns = columns)
-        with tqdm(total = end - start, desc = "Ensembling {0}".format(thread_num)) as pbar:
-            for j in range((end - start) // batch_size + 1):
-                data.get_data('train', start + j * batch_size, start + (j + 1) * batch_size if start + (j + 1) * batch_size < end else end)
-                for i, img in enumerate(data.x):
-                    stats = [brightness(img), fourier_sharpness(img), canny_sharpness(img.astype(np.uint8))]
-                    if -1 in stats:
-                        print("Failed at index {0} in batch {1}".format(i, j))
-                        errors.append(i, j)
-                        continue
-                    k = None
-                    if stats[0] < 0.203:
-                        if stats[1] < 0.172:
-                            if stats[2] < 0.0434:
-                                k = 0
-                            else:
-                                k = 1
-                        else:
-                            if stats[2] < 0.0434:
-                                k = 2
-                            else:
-                                k = 3
-                    if stats[0] > 0.203 and stats[0] < 0.382:
-                        if stats[1] < 0.172:
-                            if stats[2] < 0.0434:
-                                k = 4
-                            else:
-                                k = 5
-                        else:
-                            if stats[2] < 0.0434:
-                                k = 6
-                            else:
-                                k = 7
-                    if stats[0] > 0.382:
-                        if stats[1] < 0.172:
-                            if stats[2] < 0.0434:
-                                k = 8
-                            else:
-                                k = 9
-                        else:
-                            if stats[2] < 0.0434:
-                                k = 10
-                            else:
-                                k = 11
-                    
-                    process_df[k].loc[index[k]] = [data.names[i], data.bounds[i]]
-                    index[k] += 1
-                    pbar.update(1)
-        for i, d in enumerate(process_df):
-            df[i] = df[i].append(d.dropna(), ignore_index = True)
     
     manager = Manager()
     df = manager.list(classes)
@@ -84,7 +86,7 @@ def ensemble(batch_size, total = -1, n_threads = 1):
     for i in range(n_threads):
         start = int(i / n_threads * total)
         end = int((i + 1) / n_threads * total)
-        threads.append(Process(target = thread, args = (start, end, df, index, i)))
+        threads.append(Process(target = partial(thread, batch_size = batch_size, total = total, n_threads = n_threads), args = (start, end, df, index, i)))
 
     for i in threads:
         i.start()
@@ -95,4 +97,5 @@ def ensemble(batch_size, total = -1, n_threads = 1):
     for i, c in enumerate(df):
         c.to_csv('image_classes/class_new' + str(i) + '.csv')
 
-ensemble(400, 70000, n_threads = 2)
+if __name__ == '__main__':
+    ensemble(400, 70000, n_threads = 2)
